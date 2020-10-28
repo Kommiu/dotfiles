62b7706cdcb493f7425a52b2f3e4db0f2267c72a
Roman Misiutin <roman.misiutin@gmail.com>
2020-10-28 21:05:29 +0700

    added old but working dotfiles

---
+++ old/polybar/launch.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env sh
+
+killall -q polybar
+while pgrep -u  $UID -x polybar > /dev/null; do sleep 1; done
+
+# export MONITOR1=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | head -n 1)
+# export MONITOR2=$(xrandr -q | grep " connected" | cut -d ' ' -f1 | sed -n 2p)
+
+polybar main 2>/tmp/polybar-main.log &
+#polybar second & 
